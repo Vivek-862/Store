8a708d0df1b7c8ba559ddff3c193d23fe48d8e1c
Vivek Yadav <yadav7738vivek@gmail.com>
2024-10-14 05:38:06 +0530

    frontend done

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import { useForm } from "react-hook-form";
 const Login = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div>
        
     <dialog id="my_modal_3" className="modal">
   <     div className="modal-box dark:bg-slate-800 dark:text-white">
-        <form method="dialog">
+        <form onSubmit={handleSubmit(onSubmit)} method="dialog">
         {/* if there is a button in form, it will close the modal */}
-        <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
-        </form>
+        <Link to='/' className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
         <h3 className="font-bold text-2xl">Login</h3>
         {/* email */}
         <div className='flex flex-col mt-4'>
             <span className='justify-items-start text-left p-1'>Email</span>
             <input type="email"
             placeholder='Enter your Email'
-            className='px-3 border rounded-md outline-none text-green-500' />
+            className='px-3 border rounded-md outline-none text-green-500'
+            {...register("email", { required: true })}
+            />
+            {errors.email && <span className='text-sm text-red-500 justify-items-start text-left px-2'>This field is required</span>}
         </div>
         {/* password */}
         <div className='flex flex-col mt-4'>
             <span className='justify-items-start text-left p-1'>Password</span>
             <input type="password"
             placeholder='Enter your Password'
-            className='px-4 border rounded-md outline-none text-green-500' />
+            className='px-4 border rounded-md outline-none text-green-500'
+            {...register("password", { required: true })}
+            />
+  
+            {errors.password && <span className='text-sm text-red-500 justify-items-start text-left px-2'>This field is required</span>}
         </div>
         {/* login button */}
          <div className="flex justify-between mt-4 mx-5">
@@ -34,6 +43,8 @@ const Login = () => {
                 </Link>{" "}
             </p>
          </div>
+        </form>
+       
          </div>
      </dialog>
     </div>
--- Frontend/src/components/Signup/Signup.jsx
+++ Frontend/src/components/Signup/Signup.jsx
@@ -1,32 +1,48 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import Login from '../Login/Login'
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div>
       <div className='flex h-screen items-center justify-center '>
        
        <div  className="w-[600px]">
      <     div className="modal-box  dark:bg-slate-800 dark:text-white">
-           <form method="dailog">
+           <form onSubmit={handleSubmit(onSubmit)} method="dailog">
            {/* if there is a button in form, it will close the modal */}
            <Link to='/' className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
-           </form>
+          
            <h3 className="font-bold text-2xl">SignUp</h3>
+           {/* name */}
+           <div className='flex flex-col mt-4'>
+               <span className='justify-items-start text-left p-1'>Name</span>
+               <input type="name"
+               placeholder='Enter your Name'
+               className='px-3 border rounded-md outline-none text-green-500'
+               {...register("name", { required: true })} />
+               {errors.name && <span className='text-sm text-red-500 justify-items-start text-left px-2'>This field is required</span>}
+           </div>
            {/* email */}
            <div className='flex flex-col mt-4'>
                <span className='justify-items-start text-left p-1'>Email</span>
                <input type="email"
                placeholder='Enter your Email'
-               className='px-3 border rounded-md outline-none text-green-500' />
+               className='px-3 border rounded-md outline-none text-green-500' 
+               {...register("email", { required: true })}/>
+               {errors.email && <span className='text-sm text-red-500 justify-items-start text-left px-2'>This field is required</span>}
            </div>
            {/* password */}
            <div className='flex flex-col mt-4'>
                <span className='justify-items-start text-left p-1'>Password</span>
                <input type="password"
                placeholder='Enter your Password'
-               className='px-4 border rounded-md outline-none text-green-500' />
+               className='px-4 border rounded-md outline-none text-green-500'
+               {...register("password", { required: true })} />
+               {errors.password && <span className='text-sm text-red-500 justify-items-start text-left px-2'>This field is required</span>}
            </div>
            {/* login button */}
             <div className=" mt-4 mx-5">
@@ -45,6 +61,7 @@ const Signup = () => {
                 </p>
               
             </div>
+            </form>
             </div>
         </div>
        </div>
